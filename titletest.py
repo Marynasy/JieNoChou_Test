046e986ab325ceb44d876a87ca3055901d615db8
Marynasy <107830397+Marynasy@users.noreply.github.com>
2022-06-24 22:20:02 +0800

    Delete titletest.py

--- titletest.py
+++
@@ -1,8 +0,0 @@
-def TitleTest (MainPage, ExpectTitle):
-  if(MainPage.title == ExpectTitle):
-    print('√\n')
-    print('当前页面标题正确')
-  else:
-    print('X\n')
-    print('当前页面标题不正确，页面标题为'+MainPage.title)
- 
